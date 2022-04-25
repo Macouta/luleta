06d80e7f6984fb62384b20ec6ebfa8fe73a02360
Telma Blanc <thomas.blanc@mamoru.io*>
2022-04-25 15:16:02 +0200

    setup decors and cockpit

---
+++ Assets/Scripts/CameraClamp.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClamp : MonoBehaviour
+{
+
+    public float mouseSensivity = 100f;
+    public Vector2 xLimit = new Vector2(-30f, 30f);
+    public Vector2 yLimit = new Vector2(-30f, 30f);
+    float xRotation = 0f;
+    float yRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
+        xRotation = Mathf.Clamp(xRotation, xLimit.x, xLimit.y);
+        yRotation = Mathf.Clamp(yRotation, yLimit.x, yLimit.y);
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
