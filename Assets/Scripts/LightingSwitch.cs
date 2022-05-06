963d2b074f3983246cee5cd232124aa1fc1f4c17
tanina <jjtanina@gmail.com>
2022-05-06 10:34:45 +0200

    c bon le script des lights marche et tt bien, le jump aussi c'est tweek jcrois

--- Assets/Scripts/LightingSwitch.cs
+++ Assets/Scripts/LightingSwitch.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class LightingSwitch : MonoBehaviour
 {
-    private Light[] lights;
-
-    public float neutral_intensity;
-    public float fictional_intensity;
-
-    public float neutral_temperature;
-    public float fictional_temperature;
+    private Dictionary<Light, float> lights_info = new Dictionary<Light, float>();
 
     public GameObject neut_light;
     public GameObject fic_light;
@@ -20,10 +14,14 @@ public class LightingSwitch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*recup toutes les infos des lights pour les passer */
+        Light[] lights = FindObjectsOfType(typeof(Light)) as Light[];
+        foreach (Light current_light in lights)
+        {
+            lights_info.Add(current_light, current_light.intensity);
+        }
         neut_light.SetActive(true);
         fic_light.SetActive(false);
-        StartCoroutine(neutralLighting(duration));
+        StartCoroutine(switchLighting(duration));
     }
 
     // Update is called once per frame
@@ -36,44 +34,26 @@ public class LightingSwitch : MonoBehaviour
     {
         neut_light.SetActive(true);
         fic_light.SetActive(false);
-        StartCoroutine(neutralLighting(duration));
+        StartCoroutine(switchLighting(duration));
     }
 
     public void launchFictionalLighting()
     {
         neut_light.SetActive(false);
         fic_light.SetActive(true);
-        StartCoroutine(fictionalLighting(duration));
+        StartCoroutine(switchLighting(duration));
     }
 
-    IEnumerator neutralLighting(float duration)
+    private IEnumerator switchLighting(float duration)
     {
         float counter = 0f;
-        lights = FindObjectsOfType(typeof(Light)) as Light[];
+        Light[] lights = FindObjectsOfType(typeof(Light)) as Light[];
 
         while (counter < duration)
         {
             foreach (Light current_light in lights)
             {
-                current_light.intensity = Mathf.Lerp(fictional_intensity, neutral_intensity, tweening(counter / duration));
-                current_light.colorTemperature = Mathf.Lerp(fictional_temperature, neutral_temperature, tweening(counter / duration));
-            }
-            counter += Time.deltaTime;
-            yield return null;
-        }
-    }
-
-    IEnumerator fictionalLighting(float duration)
-    {
-        float counter = 0f;
-        lights = FindObjectsOfType(typeof(Light)) as Light[];
-
-        while (counter < duration)
-        {
-            foreach (Light current_light in lights)
-            {
-                current_light.intensity = Mathf.Lerp(neutral_intensity, fictional_intensity, tweening(counter / duration));
-                current_light.colorTemperature = Mathf.Lerp(neutral_temperature, fictional_temperature, tweening(counter / duration));
+                current_light.intensity = Mathf.Lerp(0, lights_info[current_light], tweening(counter / duration));
             }
             counter += Time.deltaTime;
             yield return null;
